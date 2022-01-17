8dbaed1c726352d332ddc6c621a24d39c534bb78
Lorrane Machado <lorranemachado21@gmail.com>
2022-01-16 21:18:55 -0300

    Show score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,16 +1,19 @@
 import MovieStars from "components/MovieStars";
 import './styles.css'
 
+type Props = {
+    score:number;
+    count:number;
+}
 
-function MovieScore() {
+function MovieScore({score, count}:Props) {
 
-    const score = 3.8;
-    const count = 17;
+   
 
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars />
+            <MovieStars score={score} />
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
     );
